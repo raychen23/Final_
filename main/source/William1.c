b2994bc048a83952e6815a71675e44fe84a1b785
WilliamNg104 <114209234+WilliamNg104@users.noreply.github.com>
2024-01-04 00:48:52 +0900

    Create William1.c

---
+++ main/source/William1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<stdbool.h>
+#pragrma warning( disable : 4996 )
+
+void displayGameStats(int attempts) {
+    printf("遊戲統計：\n");
+    printf("總嘗試次數：%d\n", attempts);
+}
